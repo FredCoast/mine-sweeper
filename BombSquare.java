2d19db1d0db05e4abb95e5f898d25e0915d23b42
FredCoast <f.cobb@lancaster.ac.uk>
2023-11-10 12:26:15 +0000

    Added comments

--- BombSquare.java
+++ BombSquare.java
@@ -35,22 +35,39 @@ public class BombSquare extends GameSquare
 	 * Returns nothing
 	 */
 	public void clicked() {
+		//Says that this square already has been "clicked"
 		checked = true;
+		//If the square contains a bomb it sets the picture to the bomb picture
 		if (thisSquareHasBomb) {
 			setImage("images/bomb.png");
 		} else {
+			//If the square does not contain a bomb it checks how many bombs are around it
 			squareScore = checkAround(x, y);
+			//It then sets the image to the quantity of bombs aound it
 			setImage("images/" + squareScore + ".png");
+			//If there are no bombs around it it starts unveiling other blank squares
 			if(squareScore == 0){
-				for(int i = -1;i<2;i++){
-					for(int j = -1;j<2;j++){
-						try{
-							BombSquare sqr = (BombSquare) board.getSquareAt(x+i, y+j);
-							if(sqr.squareScore == 0 &&!sqr.checked) sqr.clicked();
-						}catch(Exception e){
+				revealBlank();
+			}
+		}
+	}
+	/**
+	 * Reveals all the squares around the current blank one
+	 * If another square is also blank it reveals all of those blank ones recursively
+	 * Returns nothing
+	 */
+	private void revealBlank(){
+		//Checks all of the squares around the current blank one
+		for(int i = -1;i<2;i++){
+			for(int j = -1;j<2;j++){
+				//Checks if one of the adjacent sqaures are blank
+				try{
+					//Gets the sqaure to see if it is blank
+					BombSquare sqr = (BombSquare) board.getSquareAt(x+i, y+j);
+					// If the square is blank and the square has not already been checked it clicks the square
+					if(sqr.squareScore == 0 &&!sqr.checked) sqr.clicked();
+				}catch(Exception e){
 
-						}
-					}
 				}
 			}
 		}
@@ -62,11 +79,16 @@ public class BombSquare extends GameSquare
 	 * @param checkY The y coordinate of the square that needs to be checked around
 	 * @return The amount of bombs around the square that is being checked
 	 */
+	//Checks all the adjacent squares to see if they have a bomb
 	public int checkAround(int checkX,int checkY){
-		int score =0;
+		//Creates a score of how many bombs are around the desired square
+		int score = 0;
+		//Creates a loop to check all the bombs
         for(int i = -1;i<2;i++){
+			//Adds by rows the amount of squares which have bombs in them
 			score+=getAdjacent(checkX+i, checkY)+getAdjacent(checkX+i, checkY+1)+getAdjacent(checkX+i, checkY-1);
         }
+		//Returns the amount of bombs that are around
 		return score;
     }
 	/**
@@ -78,9 +100,12 @@ public class BombSquare extends GameSquare
 	 * @return 1 if the square contains a bomb 0 if the square does not
 	 */
 	private int getAdjacent(int rX, int rY){
+		//Checks if is inside the boundaries
 		try{
+			//Returns 1 if this square has a bomb 0 elsewise
 			return ((BombSquare) board.getSquareAt(rX, rY)).thisSquareHasBomb ?  1 :  0;
 		}catch(Exception e){
+			//Returns 0 is the square is outside
 			return 0;
 		}
 
